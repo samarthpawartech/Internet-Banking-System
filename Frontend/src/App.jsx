38540545577cccc82b78d647cda62996c20f8fe3
samarthpawartech <samarth.pawar.tech@gmail.com>
2026-06-27 00:02:02 +0530

    Product section added successfully

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbars/Navbar";
 import HeroSlider from "./components/HeroSlider/HeroSlider";
+import ProductsSection from "./components/ProductsSection/ProductsSection";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSlider />
+      <ProductsSection />
     </>
   );
 }
