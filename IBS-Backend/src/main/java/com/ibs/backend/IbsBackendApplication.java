7f26d4f7c1e4a907c32b59fab1574558ce20bf69
samarthpawartech <samarth.pawar.tech@gmail.com>
2026-07-22 18:14:01 +0530

    Backend Spring Boot added successfully

---
+++ IBS-Backend/src/main/java/com/ibs/backend/IbsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ibs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbsBackendApplication.class, args);
+	}
+
+}
