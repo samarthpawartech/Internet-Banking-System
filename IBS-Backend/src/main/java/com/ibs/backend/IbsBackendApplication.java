ab8cfa487f028722f4d64b899a819cf88f757f40
samarthpawartech <samarth.pawar.tech@gmail.com>
2026-07-24 20:15:48 +0530

    Database Connected Successfully

--- IBS-Backend/src/main/java/com/ibs/backend/IbsBackendApplication.java
+++ IBS-Backend/src/main/java/com/ibs/backend/IbsBackendApplication.java
@@ -2,12 +2,16 @@ package com.ibs.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class IbsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IbsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                SpringApplication.run(IbsBackendApplication.class, args);
+
+        System.out.println("✅ Spring Boot started successfully!");
+    }
 
 }
